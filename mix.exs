8500106782ca38c230cfce32a1930dddc95d8353
João Thallis <joaothallis@icloud.com>
2021-09-25 19:58:21 -0300

    Readd deps

--- mix.exs
+++ mix.exs
@@ -10,7 +10,18 @@ defmodule NameOfTheDay.MixProject do
       compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        "coveralls.github": :test
+      ],
+      dialyzer: [
+        plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+      ]
     ]
   end
 
@@ -49,7 +60,12 @@ defmodule NameOfTheDay.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
+      {:excoveralls,
+       git: "https://github.com/joaothallis/excoveralls",
+       branch: "support-elixir-1.13",
+       only: :test}
     ]
   end
 
